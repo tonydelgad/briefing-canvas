a8309bf93c3c20d2736e0f274ec9a133b6cd5a26
Tony Delgado <tonydelgado@Tonys-MacBook-Air.local>
2026-02-03 19:43:13 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <main style={{ padding: 40 }}>
+      <hi>Briefing Canvas Pilot</hi>
+      <p>If you can see this, deployment worked.</p>
+      </main>
+};
+}
