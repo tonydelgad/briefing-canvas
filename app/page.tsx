f83fed2a0ae3b4c5ea91a6eed23e0a27e83c6081
Tony Delgado <tonydelgado@Tonys-MacBook-Air.local>
2026-02-04 13:30:58 -0500

    Fix page syntax error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-      <main style={{ padding: 40 }}>
-      <hi>Briefing Canvas Pilot</hi>
+    <main style={{ padding: 40 }}>
+      <h1>Briefing Canvas Pilot</h1>
       <p>If you can see this, deployment worked.</p>
-      </main>
-};
+    </main>
+  );
 }
